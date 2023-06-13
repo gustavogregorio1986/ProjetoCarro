213957641a353e794ccbe1d9a761516382b053e0
guga56 <gustavogregorio086@gmail.com>
2023-06-13 17:21:46 -0300

    criar injeção de dependencia

--- ProjetoCarroAPI/ProjetoCarroAPI/Controllers/CarroController.cs
+++ ProjetoCarroAPI/ProjetoCarroAPI/Controllers/CarroController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjetoCarroAPI.Data.DTO;
 using ProjetoCarroAPI.Dominio.Dominio;
+using ProjetoCarroAPI.Servico.Servico;
 using ProjetoCarroAPI.Servico.Servico.Interface;
 
 namespace ProjetoCarroAPI.Controllers
@@ -11,11 +12,11 @@ namespace ProjetoCarroAPI.Controllers
     public class CarroController : ControllerBase
     {
 
-        private readonly ICarroServico carroServico;
-        private readonly IPessoaServico pessoaServico;
-        private readonly IPagamentoServico pagamentoServico;
+        private readonly CarroServico carroServico;
+        private readonly PessoaServico pessoaServico;
+        private readonly PagamentoServico pagamentoServico;
 
-        public CarroController(ICarroServico carroServico, IPessoaServico pessoaServico, IPagamentoServico pagamentoServico)
+        public CarroController(CarroServico carroServico, PessoaServico pessoaServico, PagamentoServico pagamentoServico)
         {
             this.carroServico = carroServico;
             this.pessoaServico = pessoaServico;
@@ -30,6 +31,20 @@ namespace ProjetoCarroAPI.Controllers
             pagamento.Pessoa = new Pessoa();
             CarroDTO carroDTO = new CarroDTO();
             pagamento.Pessoa.Carro = new Carro();
+            pagamentoDTO.PrecoDTO = pagamento.Preco;
+            pagamentoDTO.QtdeComprasDTO = pagamento.QtdeServicos;
+            pessoaDTO.NomePessoaDTO = pagamento.Pessoa.NomePessoa;
+            pessoaDTO.EmailPessoaDTO = pagamento.Pessoa.EmailPessoa;
+            pessoaDTO.SexoDTO = pagamento.Pessoa.Sexo;
+            pessoaDTO.CpfDTO = pagamento.Pessoa.Cpf;
+            pessoaDTO.TelefoneDTO = pagamento.Pessoa.Telefone;
+            carroDTO.NomeMarcaDTO = pagamento.Pessoa.Carro.NomeCarro;
+            carroDTO.marcaDTO = pagamento.Pessoa.Carro.Marca;
+            carroDTO.CorDTO = pagamento.Pessoa.Carro.Cor;
+            carroDTO.DescricaoDTO = pagamento.Pessoa.Carro.Descricao;
+            carroServico.Adicionar(carroDTO);
+            pessoaServico.Adicionar(pessoaDTO);
+            pagamentoServico.Adicionar(pagamentoDTO);
             return new JsonResult(pagamentoDTO);
         }
     }
